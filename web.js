885517a5dfa96dda88a63cf9b9d1f960e84e9be6
Roederick <rodrigoleonavas@gmail.com>
2013-07-16 02:27:37 +0000

    corrigindo erros

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var index = fs.readFileSync('./index.html');
+var index = fs.readFileSync('./index.html', 'utf-8');
 
 app.get('/', function(request, response) {
   response.send(index);
